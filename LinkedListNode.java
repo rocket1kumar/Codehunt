83a3f42545064cd3dfcf8bcaf5bb3c0a68cc2378
Vipul Kumar Maurya <rocket1kumar.live@gmail.com>
2018-01-15 15:19:17 +0530

    LinkedListNode.java
    This File creates a node for LinkedList

---
+++ LinkedListNode.java
@@ -0,0 +1,16 @@
+package DSJ;
+
+// Create Public Class LinkedListNode
+public class LinkedListNode {
+ 
+	// create two field for data and ref variable of the LinkedListNode
+	 int data;
+	 LinkedListNode link;
+ 
+	// through constructor initialize the field variables
+ LinkedListNode(int x){
+	 data=x;
+	 link=null;
+ }
+	
+}
